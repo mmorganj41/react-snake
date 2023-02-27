42bacd3b1943630bfd4a6d055089660e8060ec5d
mmorganj41 <mmorganj41@gmail.com>
2023-02-27 14:47:47 -0500

    added gameover check

--- src/App.tsx
+++ src/App.tsx
@@ -91,6 +91,57 @@ export default function App() {
     })
   }
 
+  function checkCollison(snake:Snake) {
+    let newSnake = [...snake];
+    let head = newSnake.shift();
+
+    if (!head) return true;
+
+    let [headX, headY] = head;
+
+    if (headX <= 0 || headX >= 50 || headY <= 0 || headY >= 50 ) {
+      return true;
+    } else if (newSnake.some(([x, y]) => x === headX && y === headY)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function stateReducer(draft: State, action: Action) {
+  switch (action.type) {
+    case 'move': {
+      if (!draft.game) return;
+      let snakeHead = draft.snake[0];
+      let newHead: SnakeSegment;
+      switch (draft.direction) {
+        case 'down':
+          newHead = [snakeHead[0], snakeHead[1]+1];
+          break;
+        case 'up':
+          newHead = [snakeHead[0], snakeHead[1]-1];
+          break;
+        case 'left':
+          newHead = [snakeHead[0]-1, snakeHead[1]];
+          break;
+        default:
+          newHead = [snakeHead[0]+1, snakeHead[1]];
+      }
+      draft.snake.pop();
+      draft.snake.unshift(newHead);
+      if (checkCollison(draft.snake)) draft.game = false;
+      break;
+    }
+    case 'control': {
+      if (!draft.game) return;
+      if (!(draft.direction === action.direction || movementDictionary[draft.direction] === action.direction)) {
+        draft.direction = action.direction;
+      }
+      break;
+    }
+  }
+}
+
   return (
     <div className="App">
       <div className="container">
@@ -111,34 +162,3 @@ type Action = {
 }
 
 
-function stateReducer(draft: State, action: Action) {
-  switch (action.type) {
-    case 'move': {
-      let snakeHead = draft.snake[0];
-      let newHead: SnakeSegment;
-      switch (draft.direction) {
-        case 'down':
-          newHead = [snakeHead[0], snakeHead[1]+1];
-          break;
-        case 'up':
-          newHead = [snakeHead[0], snakeHead[1]-1];
-          break;
-        case 'left':
-          newHead = [snakeHead[0]-1, snakeHead[1]];
-          break;
-        default:
-          newHead = [snakeHead[0]+1, snakeHead[1]];
-      }
-    
-      draft.snake.pop();
-      draft.snake.unshift(newHead);
-      break;
-    }
-    case 'control': {
-      if (!(draft.direction === action.direction || movementDictionary[draft.direction] === action.direction)) {
-        draft.direction = action.direction;
-      }
-      break;
-    }
-  }
-}
