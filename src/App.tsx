40ae8a93f85274e9c463a05bb6eb9031abbc9e05
mmorganj41 <mmorganj41@gmail.com>
2023-02-27 15:09:58 -0500

    snake grows

--- src/App.tsx
+++ src/App.tsx
@@ -81,82 +81,6 @@ export default function App() {
     })
   }
 
-
-
-  function stateReducer(draft: State, action: Action) {
-    switch (action.type) {
-      case 'move': {
-        if (!draft.game) return;
-        let snakeHead = draft.snake[0];
-        let newHead: SnakeSegment;
-        switch (draft.direction) {
-          case 'down':
-            newHead = [snakeHead[0], snakeHead[1]+1];
-            break;
-          case 'up':
-            newHead = [snakeHead[0], snakeHead[1]-1];
-            break;
-          case 'left':
-            newHead = [snakeHead[0]-1, snakeHead[1]];
-            break;
-          default:
-            newHead = [snakeHead[0]+1, snakeHead[1]];
-        }
-        draft.snake.pop();
-        draft.snake.unshift(newHead);
-        if (touchedFood(newHead, draft.food)) {
-          draft.food = generateFood();
-        }
-        if (checkCollison(draft.snake)) draft.game = false;
-        break;
-      }
-      case 'control': {
-        if (!draft.game) return;
-        if (!(draft.direction === action.direction || movementDictionary[draft.direction] === action.direction)) {
-          draft.direction = action.direction;
-        }
-        break;
-      }
-    }
-
-    function checkCollison(snake:Snake) {
-      let newSnake = [...snake];
-      let head = newSnake.shift();
-
-      if (!head) return true;
-
-      let [headX, headY] = head;
-
-      if (headX < 0 || headX >= 50 || headY < 0 || headY >= 50 ) {
-        return true;
-      } else if (newSnake.some(([x, y]) => x === headX && y === headY)) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    
-    function touchedFood(head: SnakeSegment, food: Food) {
-      let [headX, headY] = head;
-      let [foodX, foodY] = food;
-
-      if (headX === foodX && headY === foodY) {
-        return true;
-      }
-      return false;
-    } 
-
-    function generateFood(): [number, number] {
-      let x = (Math.floor(Math.random()*48)+1);
-      let y = (Math.floor(Math.random()*48)+1);
-      if (x === draft.snake[0][0] && y === draft.snake[0][1]) {
-        return generateFood();
-      } else {
-        return [x, y];
-      }
-    }
-  }
-
   return (
     <div className="App">
       <div className="container">
@@ -176,4 +100,82 @@ type Action = {
   direction: Direction;
 }
 
+function stateReducer(draft: State, action: Action) {
+  switch (action.type) {
+    case 'move': {
+      if (!draft.game) return;
+      let snakeHead = draft.snake[0];
+      let newHead: SnakeSegment;
+      switch (draft.direction) {
+        case 'down':
+          newHead = [snakeHead[0], snakeHead[1]+1];
+          break;
+        case 'up':
+          newHead = [snakeHead[0], snakeHead[1]-1];
+          break;
+        case 'left':
+          newHead = [snakeHead[0]-1, snakeHead[1]];
+          break;
+        default:
+          newHead = [snakeHead[0]+1, snakeHead[1]];
+      }
+      draft.snake.pop();
+      draft.snake.unshift(newHead);
+      if (touchedFood(newHead, draft.food)) {
+        draft.food = generateFood();
+        growSnake(draft.snake);
+      }
+      if (checkCollison(draft.snake)) draft.game = false;
+      break;
+    }
+    case 'control': {
+      if (!draft.game) return;
+      if (!(draft.direction === action.direction || movementDictionary[draft.direction] === action.direction)) {
+        draft.direction = action.direction;
+      }
+      break;
+    }
+  }
+
+  function checkCollison(snake:Snake) {
+    let newSnake = [...snake];
+    let head = newSnake.shift();
+
+    if (!head) return true;
+
+    let [headX, headY] = head;
+
+    if (headX < 0 || headX >= 50 || headY < 0 || headY >= 50 ) {
+      return true;
+    } else if (newSnake.some(([x, y]) => x === headX && y === headY)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  function touchedFood(head: SnakeSegment, food: Food) {
+    let [headX, headY] = head;
+    let [foodX, foodY] = food;
+
+    if (headX === foodX && headY === foodY) {
+      return true;
+    }
+    return false;
+  } 
+
+  function generateFood(): [number, number] {
+    let x = (Math.floor(Math.random()*48)+1);
+    let y = (Math.floor(Math.random()*48)+1);
+    if (x === draft.snake[0][0] && y === draft.snake[0][1]) {
+      return generateFood();
+    } else {
+      return [x, y];
+    }
+  }
+
+  function growSnake(snake: Snake) {
+    snake.push([NaN, NaN]);
+  }
+}
 
--- src/components/SnakeItem/SnakeItem.tsx
+++ src/components/SnakeItem/SnakeItem.tsx
@@ -16,16 +16,19 @@ const rotationDirectionDictionary = {
 
 export default function SnakeItem(props: Props) {
     let {snake, speed, direction} = props;
-    const snakeComponent = snake.map((e, i) => (<div 
+    const snakeComponent = snake.map((e, i) => {
+        if (Number.isNaN(e[0]) || Number.isNaN(e[1])) return (<div key={i} />)
+        
+        return (<div 
             className={`Snake ${!i && 'Head'}` }
             key={i}
             style={{
                 left: `${e[0] * 2}%`,
                 top: `${e[1] * 2}%`,
                 transition: `${speed}ms linear`,
-                transform: `${!i && `rotate(${rotationDirectionDictionary[direction]})`}` 
+                transform: `${!i && `rotate(${rotationDirectionDictionary[direction]})`}`,
             }}
-        />));
+        />)});
 
     return (<>{snakeComponent}</>);
 }
